f5b4fb8ae000d98021bd19db29c2eed712befe13
srijan-aich <srijanaich22@gmail.com>
2026-03-05 04:53:33 +0000

    Added Variables and Data Types chapter

---
+++ Variables and data types/var5.py
@@ -0,0 +1,5 @@
+a = "31.2" # a is a string as it is enclosed in double quotes
+b = float(a) # a but the type should be float
+t = type(b) 
+# The type of b is float as we have converted a to float and assigned it to b
+print(t) 
